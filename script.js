283a08a6092d2ef2a101516d178452b0bdaf5ba8
Radiant <you@example.com>
2020-09-26 13:43:42 +0700

    menambah dua files

---
+++ script.js
@@ -0,0 +1,2 @@
+hitungLuasPersegiPanjang = (panjang, lebar) => document.write('Panjang:', panjang, '<br>Lebar:', lebar, '<br><br>Luas persegi panjang:', panjang * lebar);
+hitungLuasPersegiPanjang(9, 4)
